ad406b6e3b7550691a40043ac1be7841104cbb13
Grunet <sadikshahidain@gmail.com>
2020-11-18 21:37:45 -0600

    Adding in a shell inline stylesheet

--- src/_includes/index.11ty.js
+++ src/_includes/index.11ty.js
@@ -1,5 +1,15 @@
-exports.data = {
-  title: "WCAG of the Day",
+const path = require("path");
+const { promises: fs } = require("fs");
+
+const pathToCssFile = path.join(__dirname, "index.css");
+
+exports.data = async function () {
+  const cssAsString = await fs.readFile(pathToCssFile);
+
+  return {
+    title: "WCAG of the Day",
+    css: cssAsString,
+  };
 };
 
 exports.render = function (data) {
@@ -9,6 +19,7 @@ exports.render = function (data) {
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>${data.title}</title>
+      <style>${data.css}</style>
     </head>
     <body>
       ${data.content}
