623d105b7d529328e50c459e5a07d49523beb266
Grunet <sadikshahidain@gmail.com>
2020-11-19 20:43:38 -0600

    Factoring out the image shortcode into a standalone function

--- .eleventy.js
+++ .eleventy.js
@@ -2,49 +2,15 @@ const inputDir = "src";
 const outputDir = "dist";
 
 const path = require("path");
-
-const Image = require("@11ty/eleventy-img");
 const escapeHtml = require("escape-html");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.setTemplateFormats(["toml", "md"]);
 
-  eleventyConfig.addShortcode("ImageProcessingShortcode", async function (
-    pathToImage,
-    altText
-  ) {
-    if (altText === undefined) {
-      throw new Error(`Missing \`alt\` on image from: ${pathToImage}`);
-    }
-    const altAttr = escapeHtml(altText);
-
-    const stats = await Image(path.join(inputDir, pathToImage), {
-      widths: [320, 640, 960, 1200, 1800, 2400],
-      formats: ["webp", "jpeg"],
-      urlPath: "/img/",
-      outputDir: path.join(outputDir, "img"),
-    });
-    const lowestSrc = stats["jpeg"][0];
-
-    // Iterate over formats and widths
-    return `<picture>
-              ${Object.values(stats)
-                .map((imageFormat) => {
-                  return `<source 
-                            type="image/${imageFormat[0].format}" 
-                            srcset="${imageFormat
-                              .map((entry) => `${entry.url} ${entry.width}w`)
-                              .join(", ")}" 
-                          >`;
-                })
-                .join("\n")}
-              <img
-                src="${lowestSrc.url}"
-                width="${lowestSrc.width}"
-                height="${lowestSrc.height}"
-                alt="${altAttr}">
-            </picture>`;
-  });
+  eleventyConfig.addShortcode(
+    "ImageProcessingShortcode",
+    __createResponsiveImageHtmlAndFiles
+  );
 
   return {
     dir: {
@@ -53,3 +19,38 @@ module.exports = function (eleventyConfig) {
     },
   };
 };
+
+async function __createResponsiveImageHtmlAndFiles(pathToImage, altText) {
+  if (altText === undefined) {
+    throw new Error(`Missing \`alt\` on image from: ${pathToImage}`);
+  }
+  const altAttribute = escapeHtml(altText);
+
+  const Image = require("@11ty/eleventy-img");
+  const stats = await Image(path.join(inputDir, pathToImage), {
+    widths: [320, 640, 960, 1200, 1800, 2400],
+    formats: ["webp", "jpeg"],
+    urlPath: "/img/",
+    outputDir: path.join(outputDir, "img"),
+  });
+  const lowestSrc = stats["jpeg"][0];
+
+  // Iterate over formats and widths
+  return `<picture>
+            ${Object.values(stats)
+              .map((imageFormat) => {
+                return `<source 
+                          type="image/${imageFormat[0].format}" 
+                          srcset="${imageFormat
+                            .map((entry) => `${entry.url} ${entry.width}w`)
+                            .join(", ")}" 
+                        >`;
+              })
+              .join("\n")}
+            <img
+              src="${lowestSrc.url}"
+              width="${lowestSrc.width}"
+              height="${lowestSrc.height}"
+              alt="${altAttribute}">
+          </picture>`;
+}
